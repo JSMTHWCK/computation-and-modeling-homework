5e1916ef492be98d223c378c2d10b75c78f5f0b9
Jeffrey Smithwick <71730271+JSMTHWCK@users.noreply.github.com>
2021-08-24 22:39:06 +0000

    cleaned up code, removed unwanted examples

--- 001/code.py
+++ 001/code.py
@@ -1,3 +1,4 @@
+#code
 def sum_of_first_n_numbers(n):
 	sum = 0
 	while (n > 0):
@@ -5,27 +6,24 @@ def sum_of_first_n_numbers(n):
 		n -= 1
 	return sum
 
-
+#code
 def check_for_symmetry(input_string):
 	return input_string[::-1] == input_string
 
-#test
-print(check_for_symmetry('tacocat'))
 
+#code
 def convert_to_numbers(string):
-	strng = string.lower()
+	stringlowercase = string.lower()
 	alphabet = ' abcdefghijklmnopqrstuvwxyz'
 	numstring = []
 
-	for lttr in strng :
-		newnumber = (alphabet.find(lttr))
+	for lttr in stringlowercase :
+		newnumber = alphabet.find(lttr)
 		numstring.append(newnumber)
 	return numstring
 
-#test 
-print(convert_to_numbers('a cat'))
 
-#test
+#code
 def is_prime(n):
 
 	for numbers in range(2,n):
@@ -35,6 +33,4 @@ def is_prime(n):
 		else:
 			return True	
 
-#code
-print(is_prime(13))
 
