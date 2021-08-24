cf6f88cb7adf4fae0fea26479a12b1acea44bb5a
Jeffrey Smithwick <71730271+JSMTHWCK@users.noreply.github.com>
2021-08-24 22:34:38 +0000

    finished day 1! (symmetry, is prime)

--- 001/code.py
+++ 001/code.py
@@ -6,3 +6,35 @@ def sum_of_first_n_numbers(n):
 	return sum
 
 
+def check_for_symmetry(input_string):
+	return input_string[::-1] == input_string
+
+#test
+print(check_for_symmetry('tacocat'))
+
+def convert_to_numbers(string):
+	strng = string.lower()
+	alphabet = ' abcdefghijklmnopqrstuvwxyz'
+	numstring = []
+
+	for lttr in strng :
+		newnumber = (alphabet.find(lttr))
+		numstring.append(newnumber)
+	return numstring
+
+#test 
+print(convert_to_numbers('a cat'))
+
+#test
+def is_prime(n):
+
+	for numbers in range(2,n):
+		if n % numbers == 0:
+			return False
+			
+		else:
+			return True	
+
+#code
+print(is_prime(13))
+
