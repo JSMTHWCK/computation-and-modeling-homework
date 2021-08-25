0f057725c0f1ae487bcd656fd2750040889d6481
Jeffrey Smithwick <71730271+JSMTHWCK@users.noreply.github.com>
2021-08-25 18:06:30 +0000

    added all the samples (although prime seems to be broken)

---
+++ 001/tests.py
@@ -0,0 +1,21 @@
+from code import check_for_symmetry
+from code import convert_to_numbers
+from code import is_prime
+
+print(check_for_symmetry("tacocat"))
+print(check_for_symmetry("pinapple"))
+print(check_for_symmetry("kayak"))
+print(check_for_symmetry("potato"))
+print(check_for_symmetry("symmetry"))
+
+print(convert_to_numbers("tacocat"))
+print(convert_to_numbers("pinapple"))
+print(convert_to_numbers("kayak"))
+print(convert_to_numbers("potato"))
+print(convert_to_numbers("symmetry"))
+
+print(is_prime(20))
+print(is_prime(16))
+print(is_prime(86))
+print(is_prime(25))
+print(is_prime(37))
