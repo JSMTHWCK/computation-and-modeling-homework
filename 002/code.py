90c3f9adbdf165103f1ae09ea2cd387225c4e76f
Jeffrey Smithwick <71730271+JSMTHWCK@users.noreply.github.com>
2021-09-02 22:02:49 +0000

    I did something

--- 002/code.py
+++ 002/code.py
@@ -11,7 +11,6 @@ def encode(string,a,b):
 	return numstring
 
 #now we reverse it
-
 def decode(numbers,a,b):
 	alphabet = ' abcdefghijklmnopqrstuvwxyz'
 	result = ''
@@ -21,3 +20,26 @@ def decode(numbers,a,b):
 		decoded_letter = alphabet[int(decoded_number)]
 		result += decoded_letter
 	return result
+
+#unoptimized
+'''
+def bruteforce(numbers):
+	for a in range(1,101):
+		for b in range (1,101):
+			for nums in numbers:
+				
+'''
+
+def bisection_search(number,power,precision):
+	lower = 0
+	upper = number + 1
+	while (upper - lower) >= 1/(10**(precision)):
+		if ((lower + upper)/2)**(power) > number:
+			upper = (lower + upper)/2
+		elif ((lower + upper)/2)**(power) < number:
+			lower = (lower+upper)/2
+		elif ((lower + upper)/2)**(power) == number:
+			return ((lower + upper)/2)
+	return ((lower + upper)/2)
+
+print(bisection_search(2,2,5))
