8d98b1c2d8e0b6af374da4a8ca7cb2f1647538c9
Jeffrey Smithwick <71730271+JSMTHWCK@users.noreply.github.com>
2021-09-17 22:18:46 +0000

    finished merge (aka the last one)

---
+++ 004/code.py
@@ -0,0 +1,19 @@
+def merge(a,b):
+	andx = 0 
+	bndx = 0 
+	final_array = []
+	while andx <= len(a) - 1 and bndx <= len(b) - 1:
+		if a[andx] <= b[bndx]:
+			final_array.append(a[andx])
+			andx += 1
+		elif a[andx] > b[bndx]:
+			final_array.append(b[bndx])
+			bndx += 1
+
+	if andx > bndx :
+		final_array.extend(b[bndx:len(b)])
+	elif bndx < andx:
+		final_array.extend(a[andx:len(a)])
+	return final_array
+	
+print(merge([2,3,4,5,6],[1,9,10,34,21]))
