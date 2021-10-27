487923d65c146a5bc30506217c82da9bf0c6269d
JSMTHWCK <jeffrey.smithwick@gmail.com>
2021-10-27 22:27:10 +0000

    split works now

--- 005/005helpers.py
+++ 005/005helpers.py
@@ -15,3 +15,12 @@ def merge(a,b):
 	elif bndx < andx:
 		final_array.extend(a[andx:len(a)])
 	return final_array
+
+def split(input):
+    first_half = []
+    last_half = []
+    for i in range(0,int(len(input)/2)):
+        first_half.append(input[i])
+    for item in range(i+1,len(input)):
+        last_half.append(input[item])
+    return first_half,last_half
